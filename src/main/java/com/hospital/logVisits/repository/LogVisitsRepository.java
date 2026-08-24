74278ab19b8828e4f281388e9155eec29b46b13c
CarfiCadu <carficadu@gmail.com>
2026-08-24 17:52:49 -0300

    feat: Criação do LogVisitsRepository ja extendendo o JpaRepository

---
+++ src/main/java/com/hospital/logVisits/repository/LogVisitsRepository.java
@@ -0,0 +1,7 @@
+package com.hospital.logVisits.repository;
+
+import com.hospital.logVisits.model.LogVisits;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LogVisitsRepository extends JpaRepository<LogVisits, Long> {
+}

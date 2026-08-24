d9ca76046d62a15b7a64e6032a233e73352aea5c
CarfiCadu <carficadu@gmail.com>
2026-08-24 15:31:47 -0300

    feat: Criação do ExamRequestDTO alem de adicionar o @Validation no pom.xml

--- src/main/java/com/hospital/exam/dto/ExamRequestDTO.java
+++ src/main/java/com/hospital/exam/dto/ExamRequestDTO.java
@@ -1,11 +1,33 @@
 package com.hospital.exam.dto;
 
+import com.hospital.admission.model.Admission;
+import com.hospital.exam.enums.ExamType;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ExamRequestDTO {
+
+    @NotNull(message = "Data do exame é obrigatório")
+    private LocalDateTime date;
+
+    @NotBlank(message = "Nome do Exame é obrigatório")
+    private String nomeExame;
+
+    @NotNull(message = "Tipo do exame é obrigatório")
+    private ExamType examType;
+
+    @NotNull(message = "Internação é obrigatória")
+    private Long admissionId;
+
+    @NotNull(message = "Médico é obrigatório")
+    private Long doctorId;
+
 }

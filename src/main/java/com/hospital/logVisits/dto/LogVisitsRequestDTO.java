93af40fc3821b852c998aec7b60fc590fe78da80
CarfiCadu <carficadu@gmail.com>
2026-08-25 15:15:39 -0300

    fix: ajustes no LogVisitasRequestDTO

--- src/main/java/com/hospital/logVisits/dto/LogVisitsRequestDTO.java
+++ src/main/java/com/hospital/logVisits/dto/LogVisitsRequestDTO.java
@@ -8,4 +8,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LogVisitsRequestDTO {
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String name;
+
+    @NotBlank(message = "Documento é obrigatório")
+    private String document;
+
+    @NotNull(message = "Internação é obrigatória")
+    private Long admissionId;
 }

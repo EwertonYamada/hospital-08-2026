2f1560a49c1ba5237d45740f6f00e78eba250807
CarfiCadu <carficadu@gmail.com>
2026-08-25 15:19:48 -0300

    fix: ajustes no LogVisitsResponseDTO

--- src/main/java/com/hospital/logVisits/dto/LogVisitsResponseDTO.java
+++ src/main/java/com/hospital/logVisits/dto/LogVisitsResponseDTO.java
@@ -4,8 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class LogVisitsResponseDTO {
+
+    private Long id;
+    private String name;
+    private String document;
+    private LocalDateTime dateTimeIn;
+    private LocalDateTime dateTimeOut;
+    private Long admissionId;
 }
